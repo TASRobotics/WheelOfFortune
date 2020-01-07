960770bdd1d9ad518b4446b92f8d8b7eef3ca8b9
usymmij <usymmij@gmail.com>
2020-01-07 11:37:10 +0800

    sorting

--- dataset/sort80.20.py
+++ dataset/sort80.20.py
@@ -1,9 +1,11 @@
 import os
 import re
+from random import random
 
 for root, dirs, files in os.walk("./photos/"):
     for filename in files:
-        if re.search('p(.+).jpg',filename):
+        if re.search('(.+).jpg',filename):
+            print('a')
             if random() > 0.8:
-                os.rename('./photos/'+filename, './test/photos'+filename)
-                os.rename('./labels/'+filename+'.label', './test/labels'+filename+'.label')
+                os.rename('./photos/'+filename, './test/photos/'+filename)
+                os.rename('./labels/'+filename+'.label', './test/labels/'+filename+'.label')
